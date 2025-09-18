a7a86881dd1be13df64403c7ae9f2ddf10c58f9d
alielhariss <drali19900@gamil.com>
2025-09-19 02:41:09 +0300

    add loops p1 to content value

---
+++ for loop/lib/main.dart
@@ -0,0 +1,15 @@
+// void main() {
+//   for (int i = 0; i <=10; i++) {
+//     print(i);
+//   }
+// }
+
+void main() {
+  for (int i = 2; i <= 20; i += 2) {
+    print(i);
+  }
+}
+// for loop used when ypu how many times the repeatation
+// first step to make the i start from the number you want
+// second step to make the i end at the number you want
+// third step to make the i increment by the number you want
---
+++ while loop/lib/main.dart
@@ -0,0 +1,9 @@
+void main() {
+  int x = 1;
+  while (x <= 5) {
+    print(x);
+    x++;
+  }
+}
+// wile loop is used when you don`t know how many times the loop will run
+
